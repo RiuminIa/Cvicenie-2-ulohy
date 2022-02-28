c9443e8c2368b5b748bbbe1ce480ee1c3d80f917
Riuminjake <64644764+Riuminjake@users.noreply.github.com>
2022-02-28 17:06:59 +0300

    Uloha 2 complete

---
+++ Uloha2.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+/**
+ * Nap´ıˇste kr´atky program na ktorom sa nauˇc´ıte pracovaˇt s poˇlom:
+ * - Definujte jednorozmern´e pole cel´ych ˇc´ısel ˇlubovolnej veˇlkosti.
+ * - Naplˇnte cel´e pole ˇc´ıslami zadan´ymi z kl´avesnice.
+ * - Overte si spr´avnosˇt naˇc´ıtan´ych hodnˆot pomocou v´ypisu na obrazovku
+ */
+public class Uloha2 {
+    public static void start() {
+        int[] a = {1, 2, 3, 4, 5};
+        int[] b = new int[10];
+        String s;
+        Scanner scan = new Scanner(System.in);
+        s=scan.nextLine();
+        Scanner scan2=new Scanner(s);
+        int i=0;
+        while (scan2.hasNextInt()){
+            b[i]=scan2.nextInt();
+            i++;
+        }
+        for (int w=0;w<i;w++){
+            System.out.print(b[w]+" ");
+        }
+    }
+}
--- main.java
+++ main.java
@@ -1,6 +1,7 @@
 public class main {
     public static void main(String[] args) {
-        Uloha1.start();
+        //Uloha1.start();
+        Uloha2.start();
 
     }
 }
